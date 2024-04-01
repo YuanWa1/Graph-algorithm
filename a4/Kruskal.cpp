33c8ff7f79844f268c46d6f35ab87234faacdfea
Yuan <jxmwang2002@gmail.com>
2024-03-31 23:05:41 -0500

    kruskal example

--- a4/Kruskal.cpp
+++ a4/Kruskal.cpp
@@ -1,4 +1,4 @@
-``#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <algorithm>
 
@@ -15,29 +15,67 @@ struct Edge {
    int weight;
 };
 
+bool sortByWeight (const Edge& a, const Edge& b){
+    return(a.weight<b.weight);
+}
+
 // Create a single-node tree with i as the root
 void MakeSet(Node *node, int i) {
     //TODO
+    node->parent = i;
 }
 
 // Return the index of the element corresponding to the root of the tree that contains i
 int FindSet(Node *nodes, int i) {
     //TODO
+    int parent = nodes[i].parent;
+    if(parent != i){
+        parent = FindSet(nodes,parent);
+        return parent;
+    }else{
+        return i;
+    }
 }
 
 // Link the root of the tree with smaller rank to the root of the tree with larger rank
 // when uniting the trees that contain u and v
 void UnionByRank(Node *nodes, int u, int v) {
     //TODO
+    int uRank = nodes[u].rank, vRank =nodes[v].rank;
+    if(uRank == vRank){
+        vRank++;
+        nodes[u].parent = v;
+    } else if(uRank<vRank){
+        nodes[u].parent = v;
+    } else{
+        nodes[v].parent = u;
+    }
 }
 
 void Kruskal(Node *nodes, Edge *edgeList, int vertexSize, int edgeCount) {
     //TODO: Implement Kruskal's algorithm to find the minimum spanning tree.
     Edge MST[vertexSize-1];
-	
+    int numInTree = 0;
+    for (int i = 0; i < vertexSize; i++) {
+        MakeSet(&nodes[i],i);
+    }
+
+    sort(edgeList, edgeList+edgeCount,sortByWeight);
+
+    for (int i = 0; i < edgeCount; ++i) {
+        if(FindSet(nodes,edgeList[i].src) != FindSet(nodes,edgeList[i].dst)){
+            MST[numInTree] = edgeList[i];
+            numInTree++;
+            UnionByRank(nodes, FindSet(nodes,edgeList[i].src),FindSet(nodes,edgeList[i].dst));
+        }
+    }
+
 	
     //TODO: Print out the resulting minimum spanning tree.
-	
+    cout << "Edges in the MST:" << endl;
+    for(int i = 0; i < numInTree; ++i) {
+        cout << MST[i].src << " -> " << MST[i].dst << " : " << MST[i].weight << endl;
+    }
 }
 
 int main(int argc, char *argv[]){
